aad37ae593f17583a6fb876c3e031dce59c65993
zero <1986365908@qq.com>
2020-03-12 08:47:09 +0800

    有关注释修改

--- gradle-core/src/main/java/org/gradle/model/User.java
+++ gradle-core/src/main/java/org/gradle/model/User.java
@@ -1,24 +1,11 @@
 package org.gradle.model;
 
+import lombok.Data;
+
 /**
  * @author Administrator
  */
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-import lombok.experimental.Accessors;
-
-/**
- * 用户信息实体
- * @Data 添加getter、setter方法
- * @NoArgsConstructor 添加无参构造器
- * @AllArgsConstructor 添加全参构造器
- * @AllArgsConstructor 添加链式调用
- * @ToString 添加toString
- * @author zero
- **/
 @Data
 public class User {
     private Integer id;
